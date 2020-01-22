fec07a27e21d1ea2efcf9cba7393cc81124cbc9b
Ross Frankenfield <rosswf.ms@gmail.com>
2020-01-22 10:33:16 -0600

    Update Simple_GCP.R

--- Simple_GCP.R
+++ Simple_GCP.R
@@ -7,7 +7,7 @@ library(curl)
 
 install.packages("googleLanguageR")
 library(googleLanguageR)
-gl_auth("My Project 81878-69463c1f5029.json")
+gl_auth("------.json")
 
 
 gl_speech("C://Users/rwfra/Desktop/Data Science/Goog/mypodcast_64kb.wav",sampleRateHertz = 22050)$transcript
